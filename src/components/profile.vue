1a8f4ae7c1abd0642d209c15e6fd25dc12307ddf
PriticaAruldas <pritica@metapercept.com>
2020-06-04 12:25:25 +0530

    Update profile.vue

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -1,29 +1,61 @@
 <template>
-<va-app>
+<va-app
+    bg-color="#F4F5F7"
+    page-bg-color="#FFFFFF"
+    desktop-margin="0"
+    desktop-minimum-width="0"
+    desktop-sidebar-width="200"
+    desktop-minibar-width="0"
+    desktop-topbar-height="0"
+    mobile-sidebar-width="0"
+    mobile-minibar-width="0"
+    mobile-topbar-height="50"
+    :rtl="false"
+    :reverse="false"
+    :split="false"
+    :sidebar-priority="false"
+    :minibar-priority="false"
+    :topbar-priority="false"
+    :topbar-padded="false">
+
+    <!-- https://vue-atlas.com/documentation/topbar -->
+    <va-topbar theme="blue">
+      <div slot="left"> L </div>
+      <div slot="center"> C </div>
+      <div slot="right"> R </div>
+    </va-topbar>
+
+    <!-- https://vue-atlas.com/documentation/minibar -->
+    <!-- <va-minibar
+      :top-items="[{icon:'home'},{icon:'search'},{icon:'user'}]"
+      :bottom-items="[{icon:'question'}]"
+      theme="blue" /> -->
+
+    <!-- https://vue-atlas.com/documentation/sidebar -->
+    <va-sidebar
+      theme="default"
+      :compact="false"
+      :text-links="false">
+      <va-sidebar-group
+        :items="Courses"
+        :title="'Courses'"
+        :show-toggle="false" />
+    </va-sidebar>
+    <!-- https://vue-atlas.com/documentation/page -->
     <va-page :size="sm" :article="article">
-      <section>
     <va-page-header>
       <div slot="title">
        User Profile
     </div>
     <div slot="subtitle">
-        Basic Account info, Classroom Profile
+        Basic info, like your name and photo, that you use on Google services
     </div>
-    <div slot="actions">
-      <div>
-        <img data-v-073e276e="" src="https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/s72-fbw=1/photo.jpg" style="width: 50px; height: 50px; border-radius: 50%; vertical-align: bottom;">
-      </div>
-  </div>
     </va-page-header>
      <!--***********************************************************************-->
      <va-row>
        <va-column :xs="12">
           <va-card :elevation="elevation" :padding="padding">
-            <div style="background-color:#f3f4f6;">
-            <div slot="topLeft" class="cardtitle" >
-              <span class="profile_heading">Profile</span>
-              </div>
-            </div>
+            <div slot="topLeft" class="cardtitle">Profile</div>
             <va-table :hover="hover" :size="size">
               <table>
                 <thead>
@@ -31,34 +63,26 @@
                 <tbody>
                   <tr>
                     <td>
-                      <va-lozenge :uppercase="uppercase" size="sm" class="label">NAME</va-lozenge>
+                      <p class="details" ><strong>NAME</strong></p>
                     </td>
-                    <td class="profiledetail">
-                      <p class="details">Pritica</p>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <va-lozenge :uppercase="uppercase" size="sm" class="label">FULL NAME</va-lozenge>
-                    </td>
-                    <td class="profiledetail">
+                    <td style="padding-right:289px">
                       <p class="details">Pritica Aruldas</p>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      <va-lozenge :uppercase="uppercase" size="sm" class="label">EMAIL</va-lozenge>
+                      <p class="details"><strong>BIRTHDAY</strong></p>
                     </td>
-                    <td class="profiledetail">
-                      <p class="details">pritica.m.aruldas@gmail.com</p>
+                    <td>
+                      <p class="details">Add Birthday</p>
                     </td>
                   </tr>
-                  <tr>
+                   <tr>
                     <td>
-                      <span><va-lozenge :uppercase="uppercase" size="sm" class="label">CLASSROOM UID</va-lozenge></span>
+                      <p class="details"><strong>USERNAME</strong></p>
                     </td>
-                    <td class="profiledetail">
-                      <p class="details">12333333335423445</p>
+                    <td>
+                      <p class="details">Pritica</p>
                     </td>
                   </tr>
                   <tr></tr>
@@ -68,50 +92,30 @@
           </va-card>
           </va-column>
      </va-row>
-     <br>
+      <br>
      <va-row>
        <va-column :xs="12">
           <va-card :elevation="elevation" :padding="padding">
-            <div style="background-color:#f3f4f6;">
-            <div slot="topLeft" class="cardtitle" >
-              <span class="profile_heading">Account Info</span>
-              </div>
-            </div>
+            <div slot="topLeft" class="cardtitle">Contact Info</div>
             <va-table :hover="hover" :size="size">
               <table>
-                <thead>
-                </thead>
+                <!-- <thead>
+                </thead> -->
                 <tbody>
-                  <tr>
-                    <td>
-                      <va-lozenge :uppercase="uppercase" size="sm" type="default" class="label">NAME</va-lozenge>
-                    </td>
-                    <td class="detail_label">
-                      <p class="details">Pritica Aruldas</p>
-                    </td>
-                  </tr>
                  <tr>
                     <td>
-                      <va-lozenge :uppercase="uppercase" size="sm" type="default" class="label">BIRTHDAY</va-lozenge>
+                      <p class="details"><strong>EMAIL</strong></p>
                     </td>
-                    <td class="detail_label">
-                      <p class="details" style="font-style: italic">Add Birthday</p>
+                    <td>
+                      <p class="details">pritica@metapercept.com</p>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      <va-lozenge :uppercase="uppercase" size="sm" type="default" class="label">PHONE</va-lozenge>
+                      <p class="details"><strong>PHONE</strong></p>
                     </td>
-                    <td class="detail_label">
-                      <p class="details" style="font-style: italic">Add a recovery phone to keep your account secure</p>
-                    </td>
-                  </tr>
-                  <tr>
                     <td>
-                      <va-lozenge :uppercase="uppercase" size="sm" type="default" class="label">STATUS</va-lozenge>
-                    </td>
-                    <td class="detail_label">
-                     <va-lozenge :uppercase="uppercase" size="sm" type="success">VERIFIED</va-lozenge>
+                      <p class="details">Add a recovery phone to keep your account secure</p>
                     </td>
                   </tr>
                   <tr></tr>
@@ -125,30 +129,21 @@
      <va-row>
        <va-column :xs="12">
           <va-card :elevation="elevation" :padding="padding">
-            <div style="background-color:#f3f4f6;">
-            <div slot="topLeft" class="cardtitle" >
-              <span class="profile_heading">Other Info</span>
-              </div>
+            <div slot="topLeft" class="cardtitle">Choose what others see
             </div>
               <va-table :hover="hover" :size="size">
               <table>
-                <thead>
-                </thead>
+                <!-- <thead>
+                </thead> -->
                 <tbody>
                  <tr>
                     <td>
-                      <va-lozenge :uppercase="uppercase" size="sm" type="default" class="label">JOIN DATE</va-lozenge>
-                    </td>
-                    <td class="detail_label">
-                      <p class="details">Friday,May 29th 2020</p>
+                      <p class="details">You decide what personal info you make visible to others across Google services</p>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      <va-lozenge :uppercase="uppercase" size="sm" type="default" class="label">LAST LOGIN</va-lozenge>
-                    </td>
-                    <td class="detail_label">
-                      <p class="details">5 hours ago</p>
+                      <p class="details">Go to About Me</p>
                     </td>
                   </tr>
                   <tr></tr>
@@ -158,7 +153,6 @@
           </va-card>
           </va-column>
      </va-row>
-     </section>
     </va-page>
 
   </va-app>
@@ -172,7 +166,12 @@ export default {
       padding: 5,
       size: 'lg',
       hover: true,
-      uppercase: true
+      Courses: [
+        {
+          name: 'courses',
+          route: '/courses'
+        }
+      ]
     }
   }
 }
@@ -182,35 +181,10 @@ export default {
 <style scoped>
 .details{
   font-size: 12px;
-  padding: 5px ;
+  padding: 5px;
 }
 .cardtitle{
-  /* padding: 0 10px 10px 0; */
-  padding-bottom:5px;
-  font-size: 25px;
-}
-.profile_heading{
-  font-size:0.9rem;
-  padding: 0 10px;
-  font-weight: 500;
-}
-.va-table{
-  margin:0;
-  table-layout: auto;
   padding: 10px;
-}
-.va-lozenge.va-lozenge-default{
- background: transparent;
-}
-.detail_label{
-  column-width:300px;
-}
-.profiledetail{
-  column-width:450px;
-  word-break:break-word;
-  /* display: -webkit-box; */
-}
-.label{
-  padding-bottom: 5px;
+  font-size: 25px;
 }
 </style>
